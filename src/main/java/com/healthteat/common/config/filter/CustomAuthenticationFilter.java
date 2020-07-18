981da4696d6ce09b1021f6c76d16453bd9a184b6
hyeongseo <luvvboy@gmail.com>
2020-07-18 13:23:54 +0900

    CustomAuthenticationFilter 수정

--- src/main/java/com/healthteat/common/config/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/healthteat/common/config/filter/CustomAuthenticationFilter.java
@@ -1,5 +1,9 @@
 package com.healthteat.common.config.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.healthteat.web.dto.MemberLoginRequestDto;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -11,9 +15,24 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    public CustomAuthenticationFilter(AuthenticationManager authenticationManager) {
+        super.setAuthenticationManager(authenticationManager);
+    }
+
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        return super.attemptAuthentication(request, response);
+        UsernamePasswordAuthenticationToken authRequest = null;
+        try {
+            MemberLoginRequestDto loginRequest = new ObjectMapper().readValue(request.getInputStream(), MemberLoginRequestDto.class);
+            authRequest = new UsernamePasswordAuthenticationToken(loginRequest.getMember_id(), loginRequest.getMember_pw());
+        } catch (IOException exception) {
+            // 사용자가 입력한 값의 문제로 인하여 예외 발생 가능
+        }
+        setDetails(request, authRequest);
+        // AbstractAuthenticationProcessingFilter의 AuthenticationManager
+        return this.getAuthenticationManager().authenticate(authRequest);
     }
+
 
 }
